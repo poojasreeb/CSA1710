6c51160f58b91cc0573d033719c3901ad486f99c
poojasreeb <192411091.simats@saveetha.com>
2026-08-04 11:31:18 +0530

    Add files via upload

---
+++ LAB/Crypt Arithmetic Problem/Python Code.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+letters = ('S','E','N','D','M','O','R','Y')
+
+for p in permutations(range(10), len(letters)):
+    S,E,N,D,M,O,R,Y = p
+
+    if S == 0 or M == 0:
+        continue
+
+    send = 1000*S + 100*E + 10*N + D
+    more = 1000*M + 100*O + 10*R + E
+    money = 10000*M + 1000*O + 100*N + 10*E + Y
+
+    if send + more == money:
+        print("Solution Found")
+        print("SEND =", send)
+        print("MORE =", more)
+        print("MONEY =", money)
+        print("\nLetter Assignments")
+        print("S =", S)
+        print("E =", E)
+        print("N =", N)
+        print("D =", D)
+        print("M =", M)
+        print("O =", O)
+        print("R =", R)
+        print("Y =", Y)
+        break
